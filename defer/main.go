239066bf4d85422b0f51dc92b1923106ba2ec1e4
Joseph Green <joseph.peter.green@gmail.com>
2024-03-27 08:01:48 +0000

    feat: defer

---
+++ defer/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	f := createFile("/tmp/defer.txt")
+	// This defers the closing of the file to the end of the encolosing functions exectution
+	// Ensuring write file finishes
+	defer closeFile(f)
+	writeFile(f)
+}
+
+func createFile(p string) *os.File {
+	fmt.Println("creating")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func writeFile(f *os.File) {
+	fmt.Println("writing")
+	fmt.Fprintln(f, "data")
+}
+
+func closeFile(f *os.File) {
+	fmt.Println("closing")
+	err := f.Close()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
