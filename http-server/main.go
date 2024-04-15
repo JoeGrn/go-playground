4bd939052e13b61ee31eaa7a7dd48c0fcc5bf8c4
Joseph Green <joseph.peter.green@gmail.com>
2024-04-15 07:54:08 +0100

    feat: http server

---
+++ http-server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// a handler for the /hello route
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+// a handler for the /headers route
+// this handler prints all the headers in the request
+func headers(w http.ResponseWriter, r *http.Request) {
+	for name, headers := range r.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	// register the handlers
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	// start the server
+	http.ListenAndServe(":8090", nil)
+}
