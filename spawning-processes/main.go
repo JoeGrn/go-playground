f11b3c43e94960a7081f7c3440c886c0b420c94d
Joseph Green <joseph.peter.green@gmail.com>
2024-04-15 08:16:53 +0100

    feat: spawning processes

---
+++ spawning-processes/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+)
+
+func main() {
+	// date command
+	dateCmd := exec.Command("date")
+
+	// Run the command and get the output
+	dateOut, err := dateCmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("> date")
+	fmt.Println(string(dateOut))
+
+	// date doesn't have a -x flag, so this should fail
+	_, err = exec.Command("date", "-x").Output()
+	if err != nil {
+		switch e := err.(type) {
+		case *exec.Error:
+			fmt.Println("failed executing:", err)
+		case *exec.ExitError:
+			fmt.Println("command exit rc =", e.ExitCode())
+		default:
+			panic(err)
+		}
+	}
+
+	grepCmd := exec.Command("grep", "hello")
+
+	// Here we explicitly grab input/output pipes, start the process, write some input to it, read the output, and finally wait for the process to exit.
+	grepIn, _ := grepCmd.StdinPipe()
+	grepOut, _ := grepCmd.StdoutPipe()
+	grepCmd.Start()
+	grepIn.Write([]byte("hello grep\ngoodbye grep"))
+	grepIn.Close()
+	grepBytes, _ := ioutil.ReadAll(grepOut)
+	grepCmd.Wait()
+
+	fmt.Println("> grep hello")
+	fmt.Println(string(grepBytes))
+
+	// Note that when spawning commands we need to provide an explicitly delineated command and argument array, vs. being able to just pass in one command line string. If you want to spawn a full command with a string, you can use bash’s -c option:
+	lsCmd := exec.Command("bash", "-c", "ls -a -l -h")
+	lsOut, err := lsCmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("> ls -a -l -h")
+	fmt.Println(string(lsOut))
+}
